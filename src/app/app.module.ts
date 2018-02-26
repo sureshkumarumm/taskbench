0a72a8c6026a7e76abd85d5368a7645a6aaa6153
UMM Digital - WDC <ummwdc@gmail.com>
2018-02-26 20:02:10 +0530

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { HeaderComponent } from './header/header.component';
 
-
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'tasks', component: TasksComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TasksComponent,
+    HeaderComponent,
+    LoginComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {useHash:true})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
