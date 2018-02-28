12f5713ddf8de92cde255002b7e2d162acb8199d
UMM Digital - WDC <ummwdc@gmail.com>
2018-02-28 19:50:45 +0530

    Login page updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,52 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 
 
-import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: TasksComponent }
-];
+// const appRoutes: Routes = [
+//   path: '', 
+//   children: [
+//     {path: '', redirectTo: '/login', pathMatch: 'full' },
+   
+//    ],
+//   component: AppComponent
+// },
+// { path: 'login', component: LoginComponent },
+//   { path: 'tasks', component: TasksComponent }
+
+ 
+// ];
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
 ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, {useHash:true})
+    //RouterModule.forRoot(appRoutes, {useHash:true})
+    RouterModule.forRoot([
+      { 
+        
+        path: '', 
+        children: [
+          {path: '', redirectTo: '/login', pathMatch: 'full' },
+          { path: 'tasks', component: TasksComponent }
+          
+         ],
+        component: HomeComponent
+      },
+      { path: 'login', component: LoginComponent },
+     
+    ],
+    {useHash:true}
+  )
     
   ],
   providers: [],
